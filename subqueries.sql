4a71453085c4a3ac4b0d193e8853a3a5603499f5
Ayakamiu <keyixie@gmail.com>
2018-07-11 21:01:57 -0400

    SQL practices

--- subqueries.sql
+++ subqueries.sql
@@ -21,6 +21,75 @@ where salesman_id in (select distinct salesman_id from salesman where customer_i
 
 select * 
 from orders
-where purch_amt > (select avg(purch_amt) as purch_amt from orders where ord_date = '2012-10-10' group by ord_date);
+where purch_amt > (select avg(purch_amt) as purch_amt 
+	from orders 
+	where ord_date = '2012-10-10' 
+	group by ord_date);
+
+/*5. Write a query to find all orders attributed to a salesman in New york*/
+
+select * 
+from orders
+where salesman_id = (select salesman_id from salesman where city = 'New York');
+
+/*6. Write a query to display the commission of all the salesmen servicing customers in Paris*/
+
+select commission
+from salesman
+where salesman_id = (select salesman_id 
+	from orders
+	where city = 'Paris')；
+
+/*7. Write a query to display all the customers whose id is 2001 bellow the salesman ID of Mc Lyon. */
+
+select *
+from customer
+where customer_id = 2001 
+and salesman_id = (select salesman_id
+	from salesman 
+	where name = 'Mc Lyon');
+
+-- if do not use subqueries
+-- join  where nam = 'Mc Lyon'
 
 
+/*8. Write a query to count the customers with grades above New York's average.*/
+
+-- why use having? since grade is in group by?
+select grade, count(*)
+from customer 
+group by grade
+having grade > (select avg(grade) 
+	from customer
+	where city = 'New York'
+	group by city);
+-- also works
+-- why use count distinct customer_id?
+-- just the number of rows could have missing customer_id
+SELECT grade, COUNT (DISTINCT customer_id) 
+FROM customer  
+where grade > (select AVG(grade) 
+	FROM customer 
+	WHERE city = 'New York') 
+Group by grade;
+
+/*9. Write a query to display all customers with orders on October 5, 2012.*/
+
+select * 
+from customer
+where customer_id in (select distinct customer_id 
+	from orders 
+	where ord_date = '2012-10-05');
+-- solution:
+SELECT *
+FROM customer a, orders b 
+WHERE a.customer_id = b.customer_id 
+AND b.ord_date = '2012-10-05';
+-- by "all customers" inplicated all fields in both tables, thus an implicit join
+
+/*10. Write a query to display all the customers with orders issued on date 17th August, 2012.*/
+
+select *
+from customer a, orders b
+where a.customer_id = b.customer_id
+and b.ord_date = '2012-08-17';
